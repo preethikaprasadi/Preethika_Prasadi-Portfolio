64eaded5218f85d228556438cc9cdc4c05d30a24
Preethika Prasadi <preethikaprasadi@gmail.com>
2025-02-15 11:43:18 +0530

    Add section to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,6 +10,7 @@ const Header = () => {
           <li><a href="#skills">Skills</a></li>
           <li><a href="#projects">Projects</a></li>
           <li><a href="#contact">Contact</a></li>
+          <li><a href="#blog">Blog</a></li>
         </ul>
       </nav>
     </header>
