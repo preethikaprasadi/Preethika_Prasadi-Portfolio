4220ad8ea89697f3462677ba6b30b752df20ef6a
Preethika Prasadi <preethikaprasadi@gmail.com>
2025-02-16 22:44:29 +0530

    Update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,8 +9,8 @@ const Header = () => {
           <li><a href="#about">About</a></li>
           <li><a href="#skills">Skills</a></li>
           <li><a href="#projects">Projects</a></li>
+          <li><a href="#blogs">Blogs</a></li>
           <li><a href="#contact">Contact</a></li>
-          <li><a href="#blog">Blog</a></li>
         </ul>
       </nav>
     </header>
